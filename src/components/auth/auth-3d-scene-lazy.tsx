c7e4e00f6b67db14852955b710b1a0898c934e50
ShaknaRizath <shakna48@gmail.com>
2026-07-28 12:16:52 +0530

    refactor: remove unused files and components from the registrar and auth sections

--- src/components/auth/auth-3d-scene-lazy.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-export const Auth3DSceneLazy = dynamic(
-  () => import("@/components/auth/auth-3d-scene").then((mod) => mod.Auth3DScene),
-  { ssr: false }
-);
